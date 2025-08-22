95346923b835bc2357c080cb9ca3fc5e83ae1a9c
Antoine Poupart <2258455@cegepmontpetit.ca>
2025-08-22 10:12:22 -0400

    FCT : MVC et Web API Complété

---
+++ 1.1-MVCetWebAPI/MVCEtWebAPI/WebAPI/DTOs/LoginSuccessDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.DTOs
+{
+    public class LoginSuccessDTO
+    {
+        [Required]
+        public string Token { get; set; } = "";
+
+    }
+}
