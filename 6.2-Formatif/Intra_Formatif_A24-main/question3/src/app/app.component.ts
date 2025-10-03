ac7d4ea0eb8dbda7e98f14bd1acf4c6ceb0110df
Antoine Poupart <2258455@cegepmontpetit.ca>
2025-10-03 11:02:47 -0400

    PROGRES ReactiveForm

--- 6.2-Formatif/Intra_Formatif_A24-main/question3/src/app/app.component.ts
+++ 6.2-Formatif/Intra_Formatif_A24-main/question3/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, ValidationErrors, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, ValidationErrors, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -9,20 +9,28 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    standalone: true,
-    imports: [MatToolbarModule, MatIconModule, MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [MatToolbarModule, MatIconModule, MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule, MatInputModule, MatButtonModule]
 })
 export class AppComponent {
   title = 'reactive.form';
 
-  
 
-  constructor(
-    
-  ) { }
+  formGroup: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.formGroup = this.formBuilder.group(
+      {
+        name: ['', [Validators.required]],
+        roadnumber: ['', [Validators.required, Validators.min(1000), Validators.max(9999)]],
+        postalCode: ['', Validators.pattern('^[A-Z][0-9][A-Z][ ]?[0-9][A-Z][0-9]$')],
+        comments: ['', Validators.minLength(10)],
+
+      },
+    );
+  }
 }
 
 
