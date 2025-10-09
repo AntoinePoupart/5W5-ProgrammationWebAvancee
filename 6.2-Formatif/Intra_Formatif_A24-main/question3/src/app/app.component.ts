e92ef45022cb56ccbca1c9ea4e45f9ce9c151c83
Antoine Poupart <antoinepoupart@icloud.com>
2025-10-09 14:18:58 -0400

    FCT Ajout du split pour le nombre de mot min

--- 6.2-Formatif/Intra_Formatif_A24-main/question3/src/app/app.component.ts
+++ 6.2-Formatif/Intra_Formatif_A24-main/question3/src/app/app.component.ts
@@ -25,11 +25,11 @@ export class AppComponent {
       {
         name: ['', [Validators.required]],
         roadnumber: ['', [Validators.required, Validators.min(1000), Validators.max(9999)]],
-        postalCode: ['', Validators.pattern('^[A-Z][0-9][A-Z][ ]?[0-9][A-Z][0-9]$')],
-        comments: ['', Validators.minLength(10)],
+        postalCode: ['', [Validators.pattern('^[A-Z][0-9][A-Z][ ]?[0-9][A-Z][0-9]$')]],
+        comments: ['', [Validators.minLength(10), this.minDixMots(10)]], //Ajouter le validateur personnalisé ici (pas plusieurs champs)
 
       },
-      { validators: this.nomDansCommentaire() }
+      { validators: this.nomDansCommentaire(), } //Si validateur sur plusieurs champs mettre ici
     );
   }
 
@@ -54,6 +54,23 @@ export class AppComponent {
       return estValide ? null : { nomDansCommentaire: true };
     };
   }
+
+  minDixMots(min: number): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const texte = control.value;
+
+      // Si le champ est vide, on ne fait pas l'erreur ici (on peut ajouter Validators.required séparément)
+      if (!texte) return null;
+
+      // Compter le nombre de mots
+      const nombreMots = texte.trim().split(/\s+/).length;
+
+      // Retourne null si valide, ou l'erreur sinon
+      //Changer le > si pour nombtre max de mots
+      return nombreMots >= min ? null : { minDixMots: { requiredWords: min, actualWords: nombreMots } };
+    };
+  }
+
 }
 
 
